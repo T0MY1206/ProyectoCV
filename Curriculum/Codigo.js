46c7762a5b1396a07459fdf3bdc85cbaedbcb2f8
Tomas Tutor Onetto <tomas.tutor@thankit.net>
2024-05-03 11:39:49 -0300

    Commit con algunas mejoras

--- Curriculum/Codigo.js
+++ Curriculum/Codigo.js
@@ -1,8 +1,1 @@
-function crearElemento()
-{
-    var contenedor = document.getElementById("contenedor");
-    var elemento = document.createElement("input");
-    var msj = document.createTextNode("Hola!¿Qué tal?");
-    contenedor.appendChild(msj);
-    contenedor.appendChild(elemento);
-}
+
